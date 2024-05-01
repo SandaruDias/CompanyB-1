893607bc5df1328ab7cb169801b38be48501fd03
SandaruDias <sandaruvihangadias@gmail.com>
2024-05-01 21:03:00 +0530

    Simulation Request Form Modifications

--- frontend/src/trainingSimulation/components03/FileUploadform.jsx
+++ frontend/src/trainingSimulation/components03/FileUploadform.jsx
@@ -1,10 +1,13 @@
 import {  useState} from "react";
-import { Button,TextField } from '@mui/material';
+import { Box, Button,TextField, Typography } from '@mui/material';
+import SimpleAlert from "./alert";
+import { Snackbar } from "@material-ui/core";
 
 function FileUploadform(){
     const [name,setName]=useState('');
     const [email,setEmail] =useState('');
     const [file,setFile] =useState(null);
+    
 
     //Change of name
     const handleNameChange = (event)=>{
@@ -38,24 +41,61 @@ function FileUploadform(){
         console.log('Name:',name);
         console.log("Email:",email);
         console.log('Uploading:',file.name);
+        alert("Successfully Submitted.")
     };
 
+
+
     return(
-        <box component="form" onSubmit={fileSubmit} noValidate={{marginTop: 20}}>
+        <Box component="form" onSubmit={handleSubmit} noValidate autoComplete="off" sx={{display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 600, margin: 'auto' ,padding: 2, 
+        border: '1px solid #ccc', 
+        borderRadius: '10px', 
+        boxShadow: '0 2px 5px rgba(0,0,0,0.1)'}}>
+            <Typography variant="h6" fontWeight="bold">SIMULATION REQUEST</Typography>
             <TextField
-              type="file"
-              onChange={fileChange}
-              inputProps={{accept :"application/pdf"}}
-              variant="outlined"
-              fullWidth
-              margin="normal"              
+                label="Name"
+                variant= "outlined"
+                value={name}
+                onChange={handleNameChange}
+                required
+                sx = {{mb:2,mt:2}}
             />
 
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 2 }}>
+                <TextField
+                    label="Email"
+                    variant= "outlined"
+                    value={email}
+                    onChange={handleEmailChange}
+                    type="email"
+                    required
+                    sx={{mb:2}}
+                />
+                <Button
+                    variant="contained"
+                    component="label"
+                    sx={{
+                        mb:2, 
+                        justifyContent: "center", 
+                        alignItems: "center", 
+                        display: "flex"
+                      }}
+                >
+                    Upload File
+                    <input
+                        type="file"
+                        hidden
+                        onChange={fileChange}
+                        accept=".pdf"
+                    />
+                </Button>
+            </Box>
+            {file && <Typography>{file.name}</Typography>}
             <Button 
-            type="submit" variant="contained" color="primary">
+            type="submit" variant="contained" color="primary" >
                 Upload Design
             </Button> 
-        </box>
+        </Box>
     );
 }
 
